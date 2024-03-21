0d00da03504ebdfb148b44732822bcb8582f1f78
kholud <kholudxz@gmail.com>
2024-03-21 15:26:32 +0300

    added the backgroundColor to all the screens

--- lib/features/shop/screens/account/accountsc.dart
+++ lib/features/shop/screens/account/accountsc.dart
@@ -4,12 +4,21 @@ class AccountScreen extends StatelessWidget {
   const AccountScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: const Center(
-        child: Text('This is the 4 screen'),
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Color(0xFFE7E6E1), Color(0xFFFAFAFA)],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text('First Screen'),
+        ),
+        body: const Center(
+          child: Text('This is the 4 screen'),
+        ),
       ),
     );
   }
--- lib/features/shop/screens/home/homesc.dart
+++ lib/features/shop/screens/home/homesc.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
 class HomeeScreen extends StatelessWidget {
- const HomeeScreen({super.key});
+  const HomeeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: const Center(
-        child: Text('This is the 1 screen'),
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Color(0xFFE7E6E1), Color(0xFFFAFAFA)],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 55, left: 40),
+                child: GestureDetector(
+                  onTap: () {
+                    // Handle tap event here
+                    // Navigate to another place or perform any action
+                  },
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: Color(0xFFE74C3C),
+                        size: 24,
+                      ),
+                      SizedBox(width: 3), // Adding some space
+                      Text(
+                        "Buraydah",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF333333),
+                            fontWeight: FontWeight.w500),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
