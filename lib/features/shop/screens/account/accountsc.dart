77c84621382c8bd457d2631f2757da86213858c4
Safdar Nazir <safdarnazir14@gmail.com>
2024-04-23 10:57:34 +0500

    implemented sign out feature

--- lib/features/shop/screens/account/accountsc.dart
+++ lib/features/shop/screens/account/accountsc.dart
@@ -1,4 +1,7 @@
+import 'package:ekra/features/Authentication/screens/login/signin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -6,16 +9,56 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child:
-            Positioned(
-              top:1500.w,
-              left: -10.w,
-              width: 20.w,
-              height: 10.h,
-              child: Image.asset("assets/images/yellow.png", fit: BoxFit.cover),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 20.w,
+            child: Image.asset(
+              "assets/images/blue.png",
+              fit: BoxFit.cover,
+              height: 40.h,
+              // width: 100.w,
             ),
-     ),
+          ),
+          Positioned(
+            top: -22.w,
+            left: -10.w,
+            // width: 40.w,
+            height: 80.h,
+            child: Image.asset(
+              "assets/images/yellow.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          if (FirebaseAuth.instance.currentUser != null)
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SafeArea(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Get.offAll(() => const SignIn());
+                  },
+                  child: const Text(
+                    "Logout",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
