386855dffe12ff7082af42cf1045475b66b50502
Safdar Nazir <safdarnazir14@gmail.com>
2024-05-05 11:29:50 +0500

    fixed when renting products showed action buttons

--- lib/features/shop/screens/myrental/order_tile.dart
+++ lib/features/shop/screens/myrental/order_tile.dart
@@ -1,6 +1,7 @@
 import 'package:ekra/common/widgets/images/rounded_image.dart';
 import 'package:ekra/features/shop/bloc/product_bloc.dart';
 import 'package:ekra/features/shop/models/order_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,6 +16,7 @@ class OrderTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bool isPending = order.status == OrderStatus.pending;
+    bool isProductMine = order.item?.userId == FirebaseAuth.instance.currentUser?.uid;
     final ProductBloc productBloc = context.watch<ProductBloc>();
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -95,7 +97,7 @@ class OrderTile extends StatelessWidget {
                         color: Colors.grey,
                       ),
                     ),
-                    if (isPending) ...[
+                    if (isPending && isProductMine) ...[
                       const SizedBox(height: 5),
                       BlocConsumer<ProductBloc, ProductState>(
                         listener: (context, state) {
